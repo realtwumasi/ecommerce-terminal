e472e6056fe76c87defdcbe308e00467cfac4f02
Michael Twumasi <therealtwumasimichael@gmail.com>
2025-03-28 13:06:40 +0000

    Updated feature XYZ

---
+++ src/register.java
@@ -0,0 +1,13 @@
+public class register {
+    String name;
+    int age;
+
+    public register(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    void resisterion() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
